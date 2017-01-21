d08bd28c4fefa0b8ccd321b792aef9b1a3c434bd
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-21 11:58:07 +0800

    migrated user model and rename application.css to scss

--- db/migrate/20170121033329_create_users.rb
+++ db/migrate/20170121033329_create_users.rb
@@ -1,6 +1,20 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :username
+      t.string :email
+      t.string :phone
+      t.string :address
+      t.string :city
+      t.string :country
+      t.string :zipcode
+      t.string :profile_pic
+
+      t.string :password
+      t.string :password_confirmation
+      t.string :password_digest
 
       t.timestamps null: false
     end
