2f072578557c5fbecfd37c4e8b09fa6c42ad7639
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-21 22:31:52 +0800

    user can login and logout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # Returns true if current_user exists, false otherwise
+	def logged_in?
+		!current_user.nil?
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
