c233ff75fe444583105809d02550602d17e9cd04
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-21 23:32:56 +0800

    user destroy done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,33 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  def edit
+    byebug
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user), notice: 'You have successfuly saved your changes.'
+    else
+      flash[:alert] = 'Cannot save your changes.'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(session[:user_id]).destroy
+    session[:user_id] = nil
+    flash[:alert] = "User account is deleted"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:firstname, :lastname, :username, :email, :phone, :address, :city, :country, :zipcode, :profile_pic, :password, :password_confirmation)
